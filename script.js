dd6e078b1228a30d7a132a5bd16cb5e8b38bb087
Noga1994 <michaelnoga94@yahoo.com>
2024-05-07 09:01:43 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+
+const getCityCoordinates = () => {
+    const cityName = cityInput.Value.trim();// Get user entered city name and remove extra spaces
+    if(!cityName) return; //Return if cityName is empty
+    //const GEOCODING_API_URL = 
+
+    console.log
+
+searchButton.addEventListener("click", getCityCoordinates)
