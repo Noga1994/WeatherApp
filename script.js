4a5a481967937d1e9c2a49f502791b94062cc06c
Noga1994 <michaelnoga94@yahoo.com>
2024-05-20 09:51:57 -0500

    no api

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 
-const API_KEY = "38314d28f3134ec48b705538241005";
+const API_KEY = ;
 
 const getCityCoordinates = () =>{
     const cityName = cityInput.value.trim();
     if(!cityName) return;
-    const GEOCODING_API_URL = 'http://api.weatherapi.com/v1/forecast.xml?key=38314d28f3134ec48b705538241005&q='+ cityName +'&days=5&aqi=no&alerts=no';
+    const GEOCODING_API_URL = 'https://api.weatherapi.com/v1/forecast.json?key=&q='+ cityName +'&days=5&aqi=no&alerts=no';
    
     fetch (GEOCODING_API_URL) .then(data => {
-        console.log(data)
+        data.json().then(json => {
+            console.log(json);
+
+            const cityNameDom = document.querySelector('#cityName');
+            cityNameDom.innerText = json.location.name;
+
+            document.querySelector('#currentIcon').src = `https://${json.current.condition.icon}`;
+            document.querySelector('#currentCondition').innerText = json.current.condition.text;
+            document.querySelector('#currentTemp').innerText = json.current.temp_f;
+            document.querySelector('#currentWind').innerText = json.current.gust_mph;
+            document.querySelector('#currentHum').innerText = json.current.humidity;
+        })
     }).catch(() =>{
         alert("An error has occurred while fetching the coordinates!")
     })
